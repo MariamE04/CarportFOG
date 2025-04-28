5c9637db3e5b38c6efbf5b29ee3700be3ddc5d71
Mariam <elmirmariam16@gmail.com>
2025-04-28 11:28:39 +0200

    domænemodel tilføjet

---
+++ src/main/java/app/entities/User.java
@@ -0,0 +1,41 @@
+package app.entities;
+
+//User-klassen er en simpel Java-klasse, der repræsenterer en bruger.
+
+public class User {
+    private int id;
+    private String email;
+    private String password;
+
+    public User(int id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
