b1206bebf249a902c2f8ff0715b358e6c872e7d7
Asger191 <ajunker@outlook.dk>
2025-04-30 11:02:47 +0200

    Rettelse på entiteter

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -7,18 +7,21 @@ public class User {
     private String email;
     private String password;
     private long PhoneNumber;
+    private String role;
 
-    public User(int id, String email, String password, long phoneNumber) {
+    public User(int id, String email, String password, long phoneNumber, String role) {
         this.id = id;
         this.email = email;
         this.password = password;
         PhoneNumber = phoneNumber;
+        this.role = role;
     }
 
     public User(String email, String password, long phoneNumber) {
         this.email = email;
         this.password = password;
         PhoneNumber = phoneNumber;
+        this.role = role;
     }
 
     public User(String email, String password) {
@@ -42,12 +45,22 @@ public class User {
         return PhoneNumber;
     }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
+                ", PhoneNumber=" + PhoneNumber +
+                ", role='" + role + '\'' +
                 '}';
     }
 }
