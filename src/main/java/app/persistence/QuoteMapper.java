1fa62b3efd4b5407b16fa38de79f9d06fec4d18e
Mariam <elmirmariam16@gmail.com>
2025-05-07 19:57:37 +0200

    i gang med tilbud (for kundens side)

--- src/main/java/app/persistence/QuoteMapper.java
+++ src/main/java/app/persistence/QuoteMapper.java
@@ -1,5 +1,6 @@
 package app.persistence;
 
+import app.entities.Order;
 import app.entities.Quote;
 import app.exceptions.DatabaseException;
 
@@ -9,7 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class QuoteMapper {
@@ -19,26 +19,63 @@ public class QuoteMapper {
         connectionPool = newConnectionPool;
     }
 
+
+    public static List<Quote> getQuotesByEmail(String email) throws DatabaseException {
+        String sql = """
+        SELECT q.quote_id, q.final_price, q.valid_until_date, q.created_at_date, q.is_accepted
+        FROM quotes q
+        JOIN orders o ON q.quote_id = o.quote_id
+        JOIN users u ON o.user_id = u.user_id
+        WHERE u.email = ?;
+        """;
+
+        List<Quote> quoteList = new ArrayList<>();
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("quote_id");
+                LocalDate validUntil = rs.getDate("valid_until_date").toLocalDate();
+                double finalPrice = rs.getDouble("final_price");
+                LocalDate createdAt = rs.getDate("created_at_date").toLocalDate();
+                boolean isAccepted = rs.getBoolean("is_accepted");
+
+                quoteList.add(new Quote(id,validUntil, finalPrice, createdAt, isAccepted));
+            }
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl i at hente tilbud for bruger med email '" + email + "': " + e.getMessage());
+        }
+
+        return quoteList;
+    }
+
+
     public static List<Quote> getAllQuets() throws DatabaseException {
         String sql = "SELECT * FROM quotes";
         List<Quote> quoteList = new ArrayList<>();
 
-        try(Connection connection = connectionPool.getConnection();
-            PreparedStatement ps = connection.prepareStatement(sql)){
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
 
             ResultSet rs = ps.executeQuery();
 
-            while (rs.next()){
-                int id =rs.getInt("quote_id");
+            while (rs.next()) {
+                int id = rs.getInt("quote_id");
                 LocalDate validDate = LocalDate.parse(rs.getString("created_at_date"));
                 double finalPrice = rs.getDouble("final_price");
                 boolean isAccepted = rs.getBoolean("is_accepted");
 
-                quoteList.add(new Quote(id,validDate,finalPrice, isAccepted));
+                quoteList.add(new Quote(id, validDate, finalPrice, isAccepted));
 
             }
         } catch (SQLException e) {
             throw new DatabaseException("Fejl i at hente alle tilbud" + e.getMessage());
         }
+        return quoteList;
     }
 }
