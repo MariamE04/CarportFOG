ef908f91f83a05e1af3c048bc16fd66dab0924e0
Mariam <elmirmariam16@gmail.com>
2025-05-09 21:03:49 +0200

    accept virker

--- src/main/java/app/persistence/QuoteMapper.java
+++ src/main/java/app/persistence/QuoteMapper.java
@@ -54,32 +54,6 @@ public class QuoteMapper {
         return quoteList;
     }
 
-
-    public static List<Quote> getAllQuets() throws DatabaseException {
-        String sql = "SELECT * FROM quotes";
-        List<Quote> quoteList = new ArrayList<>();
-
-        try (Connection connection = connectionPool.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql)) {
-
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                int id = rs.getInt("quote_id");
-                LocalDate validDate = LocalDate.parse(rs.getString("created_at_date"));
-                double finalPrice = rs.getDouble("final_price");
-                boolean isAccepted = rs.getBoolean("is_accepted");
-
-                quoteList.add(new Quote(id, validDate, finalPrice, isAccepted));
-
-            }
-        } catch (SQLException e) {
-            throw new DatabaseException("Fejl i at hente alle tilbud" + e.getMessage());
-        }
-        return quoteList;
-    }
-
-
     public static void updateQuoteAccepted(int quoteId, boolean accepted) throws DatabaseException {
         String sql = "UPDATE quotes SET is_accepted = ? WHERE quote_id = ?";
 
