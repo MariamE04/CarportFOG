91d7becd733c5ca997085d374e46cd2ca3e6d9f1
DanielBasco <danielsgeniale@gmail.com>
2025-05-10 19:15:36 +0200

    Har ikke testet det endnu. Fjernet 2 metoder i CarportMapper fordi de var ubrugelige. Tilføjet en metode til CarportMapper

--- src/main/java/app/controllers/CarportController.java
+++ src/main/java/app/controllers/CarportController.java
@@ -34,6 +34,32 @@ public class CarportController {
         }
 
 
+        public static void sendOrder(Context ctx){
+
+            try {
+
+                //todo: Mangler if-sætning til at tjekke om currentUser er logget ind eller ej
 
 
-}
+
+                //Collects users choice of measures
+                int width = Integer.parseInt(ctx.formParam("width"));
+                int length = Integer.parseInt(ctx.formParam("length"));
+
+                //Inserts to database
+                CarportMapper.addWidthAndLength(new Carport(width,length));
+
+
+
+            } catch (DatabaseException e) {
+                throw new RuntimeException(e);
+            }
+            ctx.render("createCarport.html");
+        }
+
+        }
+
+
+
+
+
--- src/main/java/app/persistence/CarportMapper.java
+++ src/main/java/app/persistence/CarportMapper.java
@@ -3,10 +3,7 @@ package app.persistence;
 import app.entities.Carport;
 import app.exceptions.DatabaseException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,54 +17,23 @@ public class CarportMapper {
     }
 
 
-    //Method to get all the widths and lengths
-    public static List<Carport> getWidthAndLength() throws DatabaseException {
-        List<Carport> widthAndLength = new ArrayList<>(); // List
-        String sql = "SELECT * FROM public.carports"; // SQL query to search for the widths and lengths
+    public static void addWidthAndLength(Carport carport) throws DatabaseException {
+        String sql = "INSERT INTO public.carports (carport_width, carport_length, carport_height, roof_type, shed_id)"+
+                "VALUES(?,?,200,fladt,null)";
 
-        try (
-                Connection connection = connectionPool.getConnection(); // Acquire a database connection.
-                PreparedStatement ps = connection.prepareStatement(sql) // Prepare the SQL statement.
-        ) {
-            ResultSet rs = ps.executeQuery(); // Execute the query and fetch results.
-            while (rs.next()) { // Iterate over each row in the result set.
-                int width = rs.getInt("carport_width"); // Extract the 'width' field from the row.
-                int length = rs.getInt("carport_length"); // Extract the 'length' field.
 
-                // Create a Carport object and add it to the list.
-                widthAndLength.add(new Carport(width, length));
-            }
-            //Catch exceptions
-        } catch (SQLException e) {
-            throw new DatabaseException("Error", e.getMessage());
+        try(Connection connection = connectionPool.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql)){
+
+            ps.setInt(1, carport.getWidth());
+            ps.setInt(2, carport.getLength());
+
+            ps.executeUpdate();
+
+        }catch (SQLException e){
+            throw new DatabaseException("Fejl med at indsætte carport mål", e.getMessage());
         }
-        return widthAndLength;  // Return the list of widthAndLength records
-    }
 
-    //Method to get the width the user chooses
-    public static Carport getChosenWidthAndLength(int choice1, int choice2) throws DatabaseException {
-        Carport carport = null; // Object to hold topping record.
-        String sql = "SELECT * FROM public.carports WHERE carport_width = ? AND carport_length = ?"; // SQL query to fetch all records.
-
-        try (
-                Connection connection = connectionPool.getConnection(); // Acquire a database connection.
-                PreparedStatement ps = connection.prepareStatement(sql) // Prepare the SQL statement.
-        ) {
-            ps.setInt(1, choice1);
-            ps.setInt(2, choice2);
-            ResultSet rs = ps.executeQuery(); // Execute the query and fetch results.
-            while (rs.next()) { // Iterate over each row in the result set.
-                int width = rs.getInt("carport_width"); // Extract the 'topping' field from the row.
-                int length = rs.getInt("carport_length"); // Extract the 'price' field.
-
-                // Create a Carport object and add it to the list.
-                carport = new Carport(width, length);
-            }
-            //Catch exceptions
-        } catch (SQLException e) {
-            throw new DatabaseException("Error", e.getMessage());
-        }
-        return carport;  // Return the list of carport records
     }
 
 }
