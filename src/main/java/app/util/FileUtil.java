369a2a7a40770ffb60cef10ab74c5f4d410981e8
Mariam <elmirmariam16@gmail.com>
2025-05-16 20:08:55 +0200

    pdf virker rigtigt

--- src/main/java/app/util/FileUtil.java
+++ src/main/java/app/util/FileUtil.java
@@ -2,22 +2,18 @@ package app.util;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 public class FileUtil {
 
-    public static String readFileFromResources(String resourcePath) {
-        ClassLoader classLoader = FileUtil.class.getClassLoader();
-        try (InputStream inputStream = classLoader.getResourceAsStream(resourcePath)) {
-            if (inputStream == null) {
-                throw new IllegalArgumentException("File not found: " + resourcePath);
-            }
-
-            return new BufferedReader(new InputStreamReader(inputStream))
-                    .lines()
-                    .collect(Collectors.joining("\n"));
+    public static byte[] readFileBytesFromProjectRoot(String relativePath) {
+        try {
+            return Files.readAllBytes(Paths.get(relativePath));
         } catch (Exception e) {
-            throw new RuntimeException("Error reading file: " + resourcePath, e);
+            throw new RuntimeException("Error reading file: " + relativePath, e);
         }
     }
 }
